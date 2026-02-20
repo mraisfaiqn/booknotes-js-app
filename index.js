5e68136ea3493255ef1008029870fc3f3900594b
Mohd Rais Faiq Nichol <mraisfaiqn@outlook.com>
2026-02-20 16:52:21 +0800

    Initial frontend structure and overview

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {    
+    res.render("index.ejs");
+});
+
+app.get("/notes", (req, res) => {    
+    res.render("notes.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Port: ${port}`);
+});
