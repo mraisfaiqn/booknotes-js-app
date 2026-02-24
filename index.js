52d986091ceaba0e58da45e71200a3856c463e9f
Mohd Rais Faiq Nichol <mraisfaiqn@outlook.com>
2026-02-24 16:25:35 +0800

    Search books by ISBN API call, capture found book cover, publish new book note and store book notes to showcase on main page in a local array.

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
 import express from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
+
+const API_URL = "https://covers.openlibrary.org/b/isbn/"
+
+let notesArray = [];
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {    
-    res.render("index.ejs");
+  res.render("index.ejs", {bookNotes: notesArray});
 });
 
-app.get("/notes", (req, res) => {    
-    res.render("notes.ejs");
+app.get("/notes", (req, res) => {
+  res.render("notes.ejs");
+});
+
+app.post("/search-book", async (req, res) => {
+  const searchISBN = req.body.isbn;
+  try {
+    const result = await axios.get(`https://openlibrary.org/api/books?bibkeys=ISBN:${searchISBN}&format=json&jscmd=data`);
+    const apiData = result.data[`ISBN:${searchISBN}`];
+    if (apiData) {
+      // Re-render the page with the found data
+      res.render("notes.ejs", { bookData: { isbn: searchISBN, cover: apiData.cover["medium"], title: apiData.title } });
+      // Handle if there is book data but no book cover image (TBC)
+    } else {
+      res.render("notes.ejs", { bookData: { isbn: searchISBN, title: "Book not found" } });
+    }
+  } catch (error) {
+    console.log(error);
+    res.render("notes.ejs", { bookData: { isbn: searchISBN, title: "URL search failed" } });
+  }
+});
+
+app.post("/publish-note", async (req, res) => {
+  const currentDate = new Date();
+  const formatDate = new Intl.DateTimeFormat('en-GB', {day: 'numeric', month: 'short', year: 'numeric'}).format(currentDate);
+  const publishDate = formatDate.toUpperCase();
+  const publishRemark = `ISBN: ${req.body.isbn} | Recommendations: ${req.body.rating}/10`;
+  const publishNote = {postDate: publishDate, postISBN: req.body.isbn, postTitle: req.body.title, postSnippet: req.body.summary, postRemark: publishRemark, postID: currentDate.getTime()};
+  notesArray.unshift(publishNote);
+  res.redirect("/");
 });
 
 app.listen(port, () => {
-    console.log(`Port: ${port}`);
+  console.log(`Port: ${port}`);
 });
